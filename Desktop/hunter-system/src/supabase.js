167ecaee067975d46487d39f0adbbc18e81b5acd
BATCODEMAN <hasan_samrae@icloud.com>
2026-05-23 10:03:32 +0200

    use env variables

--- Desktop/hunter-system/src/supabase.js
+++ Desktop/hunter-system/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://opaekocugzirfcyvpcoa.supabase.co"
-const SUPABASE_KEY = "sb_publishable_bMf8KomD94V9fb6FUKKAyg_O0zG11pr"
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
